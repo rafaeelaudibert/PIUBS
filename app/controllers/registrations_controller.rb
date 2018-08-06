19c533655b278673053572a6cae8690242b4e693
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-08-06 11:52:25 -0300

    :recycle: override registration controller from devise.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def update
+    super
+  end
+
+  private
+
+def sign_up_params
+  params.require(:user).permit(:name, :role, :sei ,:email, :password, :password_confirmation)
+end
+
+def account_update_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+end
+
+end
