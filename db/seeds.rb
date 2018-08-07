0be0c7de4e0129feec60d107215e32bd578d7442
RafaAudibert <rafaeelaudibert@gmail.com>
2018-08-07 16:55:32 -0300

    :bug: Fix city/state table
    There weren't all the cities listed in the old database. Now recovers
    the states/cities from a JSON file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-estados = (CS.states :br).sort_by { |a| a[1] }
-estados.each do |_sigla, _state|
-  _state = 'Distrito Federal' if _state == 'Federal District'
-  state = State.new('name' => _state)
-  puts `INSERTED a STATE in the database: #{_state}` if state.save
+# Insert cities and states
+JSON(IO.binread('./public/cities.json')).each do |_estado|
+  state = State.new('name' => _estado['nome'])
+  puts `INSERTED a STATE in the database: #{_estado['nome']}` if state.save
 
-  cidades = CS.cities _sigla
-  cidades.each do |_city|
+  _estado['cidades'].each do |_city|
     city = City.new('name' => _city, 'state_id' => state.id)
-    puts `INSERTED a CITY in #{_state}: #{_city}` if city.save
+    puts `INSERTED a CITY in #{_estado['nome']}: #{_city}` if city.save
   end
 end
