f71bae6a5506527031812c4ba829633372f8d19a
felipefcomerlato <felipef1909@gmail.com>
2018-08-13 15:14:57 -0300

    :construction: Testes usando a global current_user_role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-  belongs_to :company, class_name: "Company", foreign_key: :sei, optional: true 
+  belongs_to :company, class_name: "Company", foreign_key: :sei, optional: true
   enum role: [:admin, :city_admin, :city_user, :ubs_admin, :ubs_user, :company_admin, :company_user, :call_center_admin, :call_center_user]
   after_initialize :set_default_role, :if => :new_record?
+  cattr_accessor :user_logged
+  attr_accessible :user
 
   def set_default_role
-    self.role ||= :admin
+    puts $current_user_role
+
+    # temp -> new user invitation with current user role
+    self.role ||= $current_user_role
   end
 
   # Include default devise modules. Others available are:
