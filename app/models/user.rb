057c327d7cbbf00199d41a9b77f237f63928442c
felipefcomerlato <felipef1909@gmail.com>
2018-08-14 18:20:42 -0300

    :construction: listas de "roles" permitidos para cada nivel de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,34 @@
 class User < ApplicationRecord
   belongs_to :company, class_name: "Company", foreign_key: :sei, optional: true
   enum role: [:admin, :city_admin, :city_user, :ubs_admin, :ubs_user, :company_admin, :company_user, :call_center_admin, :call_center_user]
-  after_initialize :set_default_role, :if => :new_record?
-  cattr_accessor :user_logged
-  attr_accessible :user
+  after_initialize :set_default_role, :set_permitted_roles_list, :if => :new_record?
+
+  def set_permitted_roles_list
+    if $current_user_role == 'admin'
+      $permitteds = [:admin, :city_admin, :ubs_admin, :company_admin, :call_center_admin]
+    else
+      if $current_user_role == 'city_admin'
+        $permitteds = [:ubs_admin]
+      else
+        if $current_user_role == 'ubs_admin'
+          $permitteds = [:ubs_user]
+        else
+          if $current_user_role == 'company_admin'
+            $permitteds = [:company_user]
+          else
+            if $current_user_role == 'call_center_admin'
+              $permitteds = [:call_center_user]
+            else
+              $permitteds = []
+            end
+          end
+        end
+      end
+    end
+  end
 
   def set_default_role
-    puts $current_user_role
-
-    # temp -> new user invitation with current user role
-    self.role ||= $current_user_role
+    self.role ||= :admin
   end
 
   # Include default devise modules. Others available are:
