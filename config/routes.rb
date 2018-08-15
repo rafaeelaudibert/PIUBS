22157095d1cd4c996ad7444de5aa6fb5903044cb
felipefcomerlato <felipef1909@gmail.com>
2018-08-15 17:19:56 -0300

    :construction: Novas rotas criadas para o fluxo do invitation

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :cities
   resources :states
   root to: 'visitors#index'
+  get 'novo_usuario_nivel',  :to => 'users#pre_new'
+  post 'novo_usuario_nivel_selecionado',  :to => 'users#get_invitation_role'
+
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   get '404', :to => 'application#page_not_found', as: 'not_found'
