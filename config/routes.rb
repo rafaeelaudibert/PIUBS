730972daf814788c5d132650a4119b5e2bb47280
felipefcomerlato <felipef1909@gmail.com>
2018-08-15 17:54:05 -0300

    :recycle: Alteração de nomes de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :cities
   resources :states
   root to: 'visitors#index'
-  get 'novo_usuario_nivel',  :to => 'users#pre_new'
-  post 'novo_usuario_nivel_selecionado',  :to => 'users#get_invitation_role'
+  get 'new_user_level', as: 'novo_usuario_nivel',  :to => 'users#pre_new_user_invitation'
+  post 'new_user_level_selected', as:'novo_usuario_nivel_selecionado',  :to => 'users#get_invitation_role'
 
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
