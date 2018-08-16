e88e91c44829a6323ec7556cfbe26720686051bf
RafaAudibert <rafaeelaudibert@gmail.com>
2018-08-16 18:25:50 -0300

    :construction: Change how to retrieve tables, using pg_search

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,12 @@ class Answer < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :attachment
+
+  # PgSearch stuff
+  include PgSearch
+  pg_search_scope :search_for, against: {
+    question: 'A',
+    answer: 'B'
+  }, using: { tsearch: { any_word: true,
+                         prefix: true } }
 end
--- app/models/call.rb
+++ app/models/call.rb
@@ -2,6 +2,6 @@ class Call < ApplicationRecord
   belongs_to :city
   belongs_to :category
   belongs_to :state
-  belongs_to :company, class_name: "Company", foreign_key: :sei
-  has_many :replies, class_name: "Reply", foreign_key: :protocol
+  belongs_to :company, class_name: 'Company', foreign_key: :sei
+  has_many :replies, class_name: 'Reply', foreign_key: :protocol
 end
