a2d1881974e5d385fb363b4ee08fa8ca1ff69c58
felipefcomerlato <felipef1909@gmail.com>
2018-08-16 13:05:07 -0300

    :recycle: refatoração

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,26 @@
 class User < ApplicationRecord
   belongs_to :company, class_name: "Company", foreign_key: :sei, optional: true
   enum role: [:admin, :city_admin, :city_user, :ubs_admin, :ubs_user, :company_admin, :company_user, :call_center_admin, :call_center_user]
-  after_initialize :set_default_role, :set_permitted_roles_list, :if => :new_record?
+  after_initialize :set_default_role, :set_levels_allowed, :if => :new_record?
 
-  def set_permitted_roles_list
+  # Levels for new users invitateds by specific users
+  def set_levels_allowed
     if $current_user_role == 'admin'
-      $permitteds = [:admin, :city_admin, :ubs_admin, :company_admin, :call_center_admin]
+      $levels_allowed = [:admin, :city_admin, :ubs_admin, :company_admin, :call_center_admin]
     else
       if $current_user_role == 'city_admin'
-        $permitteds = [:ubs_admin]
+        $levels_allowed = [:ubs_admin]
       else
         if $current_user_role == 'ubs_admin'
-          $permitteds = [:ubs_user]
+          $levels_allowed = [:ubs_user]
         else
           if $current_user_role == 'company_admin'
-            $permitteds = [:company_user]
+            $levels_allowed = [:company_user]
           else
             if $current_user_role == 'call_center_admin'
-              $permitteds = [:call_center_user]
+              $levels_allowed = [:call_center_user]
             else
-              $permitteds = []
+              $levels_allowed = []
             end
           end
         end
@@ -28,7 +29,7 @@ class User < ApplicationRecord
   end
 
   def set_default_role
-    self.role ||= :admin
+    self.role ||= $selected_role
   end
 
   # Include default devise modules. Others available are:
