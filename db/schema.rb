298a68fb4455136ee8aef262de6a4c920764fa70
RafaAudibert <rafaeelaudibert@gmail.com>
2018-08-17 13:17:15 -0300

    :card_file_box: Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_190404) do
+ActiveRecord::Schema.define(version: 2018_08_17_111704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_08_15_190404) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "faq", default: false
     t.index ["category_id"], name: "index_answers_on_category_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
@@ -54,6 +55,8 @@ ActiveRecord::Schema.define(version: 2018_08_15_190404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "answer_id"
+    t.index ["answer_id"], name: "index_calls_on_answer_id"
     t.index ["category_id"], name: "index_calls_on_category_id"
     t.index ["city_id"], name: "index_calls_on_city_id"
     t.index ["state_id"], name: "index_calls_on_state_id"
@@ -101,6 +104,7 @@ ActiveRecord::Schema.define(version: 2018_08_15_190404) do
     t.datetime "updated_at", null: false
     t.integer "status"
     t.string "category"
+    t.boolean "faq", default: false
     t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
@@ -160,6 +164,7 @@ ActiveRecord::Schema.define(version: 2018_08_15_190404) do
   add_foreign_key "answers", "categories"
   add_foreign_key "answers", "users"
   add_foreign_key "attachments", "answers"
+  add_foreign_key "calls", "answers"
   add_foreign_key "calls", "categories"
   add_foreign_key "calls", "cities"
   add_foreign_key "calls", "companies", column: "sei", primary_key: "sei"
