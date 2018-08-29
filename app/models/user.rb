615df9180bcb090cc7905b6ebfb110c0f20cbf28
felipefcomerlato <felipef1909@gmail.com>
2018-08-29 14:55:12 -0300

    :construction: validação cpf backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :answer
   enum role: [:admin, :city_admin, :city_user, :ubs_admin, :ubs_user, :company_admin, :company_user, :call_center_admin, :call_center_user]
   after_initialize :set_default_role, :if => :new_record?
+  validates_cpf_format_of :cpf, options: {allow_blank: true, allow_nil: true}
 
   def set_default_role
     self.sei = $user_sei
