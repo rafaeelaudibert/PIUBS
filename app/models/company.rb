c52e7085f31afbec4c4f2379af1a09ff0db738df
RafaAudibert <rafaeelaudibert@gmail.com>
2018-08-30 15:15:00 -0300

    :lock: Requiring uniqueness through the back-end, not just the database

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,7 @@ class Company < ApplicationRecord
   has_many :user, class_name: 'User', foreign_key: :sei
   has_many :contracts, class_name: 'Contract', foreign_key: :sei
   has_many :call, class_name: 'Call', foreign_key: :sei
+  validates :sei, presence: true, uniqueness: true
+
   self.primary_key = 'sei' # Setting a different primary_key
 end
--- app/models/unity.rb
+++ app/models/unity.rb
@@ -1,5 +1,7 @@
 class Unity < ApplicationRecord
   belongs_to :city
   has_many :users
+  validates :cnes, presence: true, uniqueness: true
+
   self.primary_key = 'cnes'
 end
