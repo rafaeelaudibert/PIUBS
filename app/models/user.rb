9d923939271f4c6f2b70647f12c5657735123a79
RafaAudibert <rafaeelaudibert@gmail.com>
2018-09-03 18:23:01 -0300

    :bug: Fix gambiarra in User with SEI creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ApplicationRecord
-  belongs_to :company, class_name: "Company", foreign_key: :sei, optional: true
-  belongs_to :unity, class_name: "Unity", foreign_key: :cnes, optional: true
-  belongs_to :city, class_name: "City", foreign_key: :id, optional: true
+  belongs_to :company, class_name: 'Company', foreign_key: :sei, optional: true
+  belongs_to :unity, class_name: 'Unity', foreign_key: :cnes, optional: true
+  belongs_to :city, class_name: 'City', foreign_key: :id, optional: true
   has_many :calls
   has_many :answer
-  enum role: [:admin, :city_admin, :city_user, :ubs_admin, :ubs_user, :company_admin, :company_user, :call_center_admin, :call_center_user]
-  after_initialize :set_default_role, :if => :new_record?
-  validates_cpf_format_of :cpf, options: {allow_blank: true, allow_nil: true}
-
-  def set_default_role
-    self.sei = $user_sei
-  end
+  enum role: %i[admin city_admin city_user ubs_admin ubs_user company_admin company_user call_center_admin call_center_user]
+  validates_cpf_format_of :cpf, options: { allow_blank: true, allow_nil: true }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
