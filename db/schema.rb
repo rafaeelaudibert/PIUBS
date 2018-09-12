dafb322a6af2e04f423e408b0aeafa2fabe7311f
RafaAudibert <rafaeelaudibert@gmail.com>
2018-09-12 16:44:07 -0300

    :card_file_box: Adding keywords field to faq

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_130001) do
+ActiveRecord::Schema.define(version: 2018_09_12_164101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_130001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "faq", default: false
+    t.string "keywords"
     t.index ["category_id"], name: "index_answers_on_category_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
