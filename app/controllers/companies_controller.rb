362ad3313e1d1c3ce566b0e672141b3a610205e7
RafaAudibert <rafaeelaudibert@gmail.com>
2018-09-14 13:12:43 -0300

    :lock: Improve security to Companies views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,6 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: %i[show edit update destroy]
+  before_action :filter_role
   include ApplicationHelper
 
   # GET /companies
@@ -92,6 +93,16 @@ class CompaniesController < ApplicationController
     params.require(:company).permit(:sei)
   end
 
+  def filter_role
+    action = params[:action]
+    if %w[index new create destroy edit update].include? action
+      redirect_to denied_path unless is_admin?
+    elsif %w[getStates getUsers getCities getUnities].include? action
+      pp current_user
+      pp params
+      redirect_to denied_path unless is_admin? || is_support_user? || (is_company_user && params[:id].to_i == current_user.sei)
+    elsif action == 'show'
+      redirect_to denied_path unless current_user.try(:company_admin?) && @company.sei == current_user.sei
     end
   end
 end
