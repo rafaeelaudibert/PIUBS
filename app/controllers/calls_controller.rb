39532fbaeacb8df6ffee60fe069a690672bc097d
felipefcomerlato <felipef1909@gmail.com>
2018-09-17 18:00:48 -0300

    :bug: Quais calls cada usuário pode ver na lista

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -6,7 +6,15 @@ class CallsController < ApplicationController
   # GET /calls
   # GET /calls.json
   def index
-    if current_user.try(:call_center_user?) || current_user.try(:call_center_admin?) || current_user.try(:admin?)
+    if current_user.try(:call_center_user?)
+      @calls = Call.where(support_user: [current_user.id, nil]).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    elsif current_user.try(:call_center_admin?)
+      children = [nil]
+      User.where(invited_by_id: current_user.id).each do |user|
+        children << user.id
+      end
+      @calls = Call.where(support_user: children).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    elsif current_user.try(:admin?)
       @calls = Call.order(created_at: :desc).paginate(page: params[:page], per_page: 25)
     elsif current_user.try(:company_admin?)
       @calls = Call.where(sei: current_user.sei).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
