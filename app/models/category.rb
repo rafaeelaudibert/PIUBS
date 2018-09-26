89ed17ece0d3c0c83a13ca47ddb55b62863b11ac
RafaAudibert <rafaeelaudibert@gmail.com>
2018-09-26 18:33:50 -0300

    :lipstick: Update answer creation form

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,7 @@ class Category < ApplicationRecord
   validates :parent, presence: false, uniquenes: false
 
   belongs_to :parent, class_name: :Category, foreign_key: :parent_id, optional: true
+  has_many :children, ->(category) { where(parent_id: category.id) }, class_name: :Category
+
   enum severity: %i[low medium high]
 end
