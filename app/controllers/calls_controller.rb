cd16b44317d3fcc676ec5b19fb6abb81b18ac010
felipefcomerlato <felipef1909@gmail.com>
2018-09-26 17:34:11 -0300

    :recycle: :construction: Add filter in calls_controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -7,23 +7,37 @@ class CallsController < ApplicationController
   # GET /calls
   # GET /calls.json
   def index
-    if current_user.try(:call_center_user?)
-      @calls = Call.where(support_user: [current_user.id, nil]).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
-    elsif current_user.try(:call_center_admin?)
-      children = [nil]
-      User.where(invited_by_id: current_user.id).each do |user|
-        children << user.id
-      end
-      @calls = Call.where(support_user: children).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
-    elsif current_user.try(:admin?)
-      @calls = Call.order(created_at: :desc).paginate(page: params[:page], per_page: 25)
-    elsif current_user.try(:company_admin?)
-      @calls = Call.where(sei: current_user.sei).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
-    elsif current_user.try(:company_user?)
-      @calls = Call.where(user_id: current_user.id).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
-    else
-      @calls = []
-    end
+    # if current_user.try(:call_center_user?)
+    #   @calls = Call.where(support_user: [current_user.id, nil]).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    # elsif current_user.try(:call_center_admin?)
+    #   children = [nil]
+    #   User.where(invited_by_id: current_user.id).each do |user|
+    #     children << user.id
+    #   end
+    #   @calls = Call.where(support_user: children).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    # elsif current_user.try(:admin?)
+    #   @calls = Call.order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    # elsif current_user.try(:company_admin?)
+    #   @calls = Call.where(sei: current_user.sei).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    # elsif current_user.try(:company_user?)
+    #   @calls = Call.where(user_id: current_user.id).order(created_at: :desc).paginate(page: params[:page], per_page: 25)
+    # else
+    #   @calls = []
+    # end
+    @filterrific = initialize_filterrific(
+       Call,
+       params[:filterrific],
+       select_options: {
+         sorted_by: Call.options_for_sorted_by,
+       },
+       :persistence_id => false,
+     ) or return 
+     @calls = @filterrific.find.page(params[:page])
+
+     respond_to do |format|
+       format.html
+       format.js
+     end
   end
 
   # GET /calls/1
