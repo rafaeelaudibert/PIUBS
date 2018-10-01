3d74a1c6ddfe8ad81259fdc8e4af117cab16d1c4
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-10-01 14:56:20 -0300

    :recycle: fix tibyMCE bug

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module ApoioPiubs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.tinymce.install = :compile
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
