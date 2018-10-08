85cdc42938631a56a0637653f8b7c3fa13a8d936
felipefcomerlato <felipef1909@gmail.com>
2018-10-08 15:19:05 -0300

    :construction: Protótipo de pesquisa das calls

--- app/models/call.rb
+++ app/models/call.rb
@@ -28,8 +28,37 @@ class Call < ApplicationRecord
       with_company
       with_state
       with_city
+      search_query
     ]
   )
+
+  scope :search_query, lambda { |query|
+    # Searches the students table on the 'first_name' and 'last_name' columns.
+    # Matches using LIKE, automatically appends '%' to each term.
+    # LIKE is case INsensitive with MySQL, however it is case
+    # sensitive with PostGreSQL. To make it work in both worlds,
+    # we downcase everything.
+    return nil  if query.blank?
+
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
+
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+      (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+    }
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conds = 2
+    where(
+      terms.map { |term|
+        "(LOWER(calls.title) LIKE ? OR LOWER(calls.description) LIKE ?)"
+      }.join(' AND '),
+      *terms.map { |e| [e] * num_or_conds }.flatten
+    )
+  }
 
   scope :sorted_by_creation, lambda { |sort_key|
     sort = /asc$/.match?(sort_key) ? 'asc' : 'desc'
