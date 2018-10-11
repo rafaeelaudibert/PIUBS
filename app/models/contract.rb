9ee2f0bad5b25ead99e86c07dccf73c66e6054fd
felipefcomerlato <felipef1909@gmail.com>
2018-10-11 15:19:36 -0300

    :sparkles: Introduces search mechanism in the Contracts

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -6,4 +6,18 @@ class Contract < ActiveRecord::Base
   validates :contract_number, presence: true, uniqueness: true
   validates :city_id, presence: true
   validates :sei, presence: true
+
+  filterrific(
+    default_filter_params: { # em breve
+     },
+    available_filters: %i[
+      search_query
+    ]
+  )
+
+  scope :search_query, lambda { |query|
+    return nil  if query.blank?
+    query_search_i = query.to_i
+    where("contract_number = ?", query_search_i)
+  }
 end
