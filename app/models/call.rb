2f57c03a2d9c24b2c4575d7bbb5346c74824286d
felipefcomerlato <felipef1909@gmail.com>
2018-10-11 12:24:06 -0300

    :construction: Add search in Calls

--- app/models/call.rb
+++ app/models/call.rb
@@ -33,31 +33,9 @@ class Call < ApplicationRecord
   )
 
   scope :search_query, lambda { |query|
-    # Searches the students table on the 'first_name' and 'last_name' columns.
-    # Matches using LIKE, automatically appends '%' to each term.
-    # LIKE is case INsensitive with MySQL, however it is case
-    # sensitive with PostGreSQL. To make it work in both worlds,
-    # we downcase everything.
     return nil  if query.blank?
-
-    # condition query, parse into individual keywords
-    terms = query.downcase.split(/\s+/)
-
-    # replace "*" with "%" for wildcard searches,
-    # append '%', remove duplicate '%'s
-    terms = terms.map { |e|
-      (e.gsub('*', '%') + '%').gsub(/%+/, '%')
-    }
-    # configure number of OR conditions for provision
-    # of interpolation arguments. Adjust this if you
-    # change the number of OR conditions.
-    num_or_conds = 2
-    where(
-      terms.map { |term|
-        "(LOWER(calls.title) LIKE ? OR LOWER(calls.description) LIKE ?)"
-      }.join(' AND '),
-      *terms.map { |e| [e] * num_or_conds }.flatten
-    )
+    query_search = "%#{query}%"
+    where("title ILIKE :search", search: query_search)
   }
 
   scope :sorted_by_creation, lambda { |sort_key|
