a330215fc90bdc86562a79f218dc0466c26a9a39
felipefcomerlato <felipef1909@gmail.com>
2018-10-11 13:13:40 -0300

    :sparkles: Introduces search mechanism in the Unities

--- app/models/unity.rb
+++ app/models/unity.rb
@@ -7,4 +7,18 @@ class Unity < ApplicationRecord
   validates :cnes, presence: true, uniqueness: true
 
   self.primary_key = 'cnes'
+
+  filterrific(
+    default_filter_params: { # em breve
+     },
+    available_filters: %i[
+      search_query
+    ]
+  )
+
+  scope :search_query, lambda { |query|
+    return nil  if query.blank?
+    query_search = "%#{query}%"
+    where("name ILIKE :search", search: query_search)
+  }
 end
