8c06b7b0dc371d5093607085a6b083344fb5fb3e
RafaAudibert <rafaeelaudibert@gmail.com>
2018-10-18 20:49:49 +0000

    :sparkles::recycle: Removes files without links

--- db/whenever.rb
+++ db/whenever.rb
@@ -1,3 +1,8 @@
+every :sunday, at: '04am' do
+  runner 'Attachment.all.select { |attach| attach.attachment_links.length == 0 }.each { |attach| attach.destroy! }',
+          mailto: 'rbaudibert@inf.ufrgs.br'
+end
+
 every :day, at: '7:50 pm' do
   command "backup perform --trigger PIUBS_production"
 end
