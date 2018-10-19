eeaf21f4a90d2fe095a4c6c4394222fa1513a053
codefactor-io <support@codefactor.io>
2018-10-19 16:48:20 +0000

    [CodeFactor] Apply fixes

--- db/whenever.rb
+++ db/whenever.rb
@@ -4,7 +4,7 @@ every :sunday, at: '04am' do
   runner 'Attachment.all'\
                    '.select { |attach| attach.attachment_links.length == 0 }'\
                    '.each { |attach| attach.destroy! }',
-          mailto: 'rbaudibert@inf.ufrgs.br'
+         mailto: 'rbaudibert@inf.ufrgs.br'
 end
 
 every :day, at: '1:00 am' do
