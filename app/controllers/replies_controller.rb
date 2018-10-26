fa0786a83ba62cf9ee6b91031b92d7aac05e4180
RafaAudibert <rafaeelaudibert@gmail.com>
2018-10-26 10:15:11 -0300

    :rotating_light: Fixes Rubocop warnings for replies_controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -32,32 +32,13 @@ class RepliesController < ApplicationController
   # POST /replies
   def create
     rep_params = reply_params
-    files = rep_params[:files] ? rep_params.delete(:files).split(',') : []
-
-    @reply = Reply.new(rep_params)
-    @reply.user_id = current_user.id
-    @reply.status = @reply.repliable.status || 'Sem Status'
-    @reply.category = if support_user? || current_user.try(:admin?)
-                        'support'
-                      else
-                        company_user? ? 'company' : 'unity'
-                      end
+    files = retrieve_files rep_params
+    @reply = create_reply rep_params
 
     if @reply.save
-      files.each do |file_uuid|
-        @link = AttachmentLink.new(attachment_id: file_uuid, reply_id: @reply.id, source: 'reply')
-
-        unless @link.save
-          raise 'Não consegui criar o link entre arquivo e a resposta.'\
-                ' Por favor tente mais tarde'
-        end
-      end
-
+      create_file_links reply, files
       ReplyMailer.notify(@reply, current_user).deliver_later
-
-      id = @reply.repliable_id
-      redirect_to @reply.repliable_type == 'Call' ? call_path(id) : controversy_path(id),
-                  notice: 'Resposta adicionada com sucesso.'
+      redirect_to create_path(@reply), notice: 'Resposta adicionada com sucesso.'
     else
       render :new
     end
@@ -66,7 +47,7 @@ class RepliesController < ApplicationController
   # PATCH/PUT /replies/1
   def update
     if @reply.update(reply_params)
-      redirect_to @reply, notice: 'Reply was successfully updated.'
+      redirect_to @reply, notice: 'Resposta atualizada com sucesso.'
     else
       render :edit
     end
@@ -75,7 +56,7 @@ class RepliesController < ApplicationController
   # DELETE /replies/1
   def destroy
     @reply.destroy
-    redirect_to replies_url, notice: 'Reply was successfully destroyed.'
+    redirect_to replies_url, notice: 'Resposta apagada com sucesso.'
   end
 
   # GET /replies/attachments/:id
@@ -88,12 +69,7 @@ class RepliesController < ApplicationController
                        { filename: attachment.filename,
                          type: attachment.content_type,
                          id: attachment.id,
-                         bytes: Reply.connection
-                                     .select_all(Reply.sanitize_sql_array(
-                                                   ['SELECT octet_length(file_contents) FROM '\
-                                                    'attachments WHERE attachments.id = ?',
-                                                    attachment.id]
-                                                 ))[0]['octet_length'] }
+                         bytes: retrieve_file_bytes(attachment) }
                      end)
       end
     end
@@ -104,6 +80,47 @@ class RepliesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_reply
     @reply = Reply.find(params[:id])
+  end
+
+  def retrieve_files(rep_params)
+    rep_params[:files] ? rep_params.delete(:files).split(',') : []
+  end
+
+  def create_reply(rep_params)
+    reply = Reply.new(rep_params)
+    reply.user_id = current_user.id
+    reply.status = @reply.repliable.status || 'Sem Status'
+    reply.category = if support_user? || current_user.try(:admin?)
+                       'support'
+                     else
+                       company_user? ? 'company' : 'unity'
+                     end
+    reply
+  end
+
+  def create_file_links(reply, files)
+    files.each do |file_uuid|
+      @link = AttachmentLink.new(attachment_id: file_uuid, reply_id: reply.id, source: 'reply')
+
+      unless @link.save
+        raise 'Não consegui criar o link entre arquivo e a resposta.'\
+              ' Por favor tente mais tarde'
+      end
+    end
+  end
+
+  def retrieve_file_bytes(attachment)
+    Reply.connection
+         .select_all(Reply.sanitize_sql_array(
+                       ['SELECT octet_length(file_contents) FROM '\
+                        'attachments WHERE attachments.id = ?',
+                        attachment.id]
+                     ))[0]['octet_length']
+  end
+
+  def create_path(reply)
+    id = reply.repliable_id
+    reply.repliable_type == 'Call' ? call_path(id) : controversy_path(id)
   end
 
   # Never trust parameters from internet, only allow the white list through.
