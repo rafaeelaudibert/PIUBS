c0fa058af245e12d50932f0c205e622ca51e7afb
mgfzemor <mario.ufrgs.inf@gmail.com>
2018-10-31 20:55:28 -0300

    :sparkles: validates reply form.

--- app/assets/javascripts/calls.js
+++ app/assets/javascripts/calls.js
@@ -753,4 +753,26 @@
           }
       }
   });
+
+  /**
+  * validate reply form
+  */
+  $("#replyForm").submit(function(){
+    tinyMCE.triggerSave();
+  });
+  $.validator.setDefaults({
+      ignore: ":hidden:not('textarea')"
+  });
+  var validator = $('#replyForm').validate({
+      rules: {
+          "reply[description]": {
+              required: true
+          }
+      },
+      messages :{
+          "reply[description]" : {
+              required : 'O campo Descrição é obrigatório.'
+          }
+      }
+  });
 })(window.jQuery);
