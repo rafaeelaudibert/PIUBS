72fa46b425ec2230facd64db62cd4d473f4d5c5b
RafaAudibert <rafaeelaudibert@gmail.com>
2018-11-06 10:27:10 -0200

    :card_file_box: Run db:seed to configure schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2018_10_29_174347) do
+ActiveRecord::Schema.define(version: 2018_11_05_184347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
