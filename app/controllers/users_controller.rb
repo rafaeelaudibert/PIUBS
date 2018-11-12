74f8cef1b89e2ad280c61870426d67a94fbf4f76
felipefcomerlato <felipef1909@gmail.com>
2018-11-12 14:13:25 -0200

    :recycle: Refactoring users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,10 @@ class UsersController < ApplicationController
            @user == current_user
       redirect_to root_path, alert: 'Acesso Negado!'
     end
+    get_user_infos
+  end
+
+  def user_infos
     @company = Company.find(@user.sei) if @user.sei
     @unity = Unity.find(@user.cnes) if @user.cnes
     @city = City.find(@user.city_id) if @user.city_id
@@ -57,9 +61,9 @@ class UsersController < ApplicationController
 
   def allowed_users
     if admin?
-      @filterrific.find.page(params[:page]).limit(999999)
+      @filterrific.find.page(params[:page]).limit(999_999)
     elsif admin_like?
-      @filterrific.find.where(invited_by_id: current_user.id).page(params[:page]).limit(999999)
+      @filterrific.find.where(invited_by_id: current_user.id).page(params[:page]).limit(999_999)
     end
   end
 
