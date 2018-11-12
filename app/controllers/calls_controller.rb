063a315ce13a350f8cbbeb3dd5c0e0f5bb96ea37
felipefcomerlato <felipef1909@gmail.com>
2018-11-12 15:22:54 -0200

    :recycle: Refactoring filter_role

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -243,8 +243,7 @@ class CallsController < ApplicationController
       redirect_to denied_path unless admin?
     elsif %w[new create destroy].include? action
       redirect_to denied_path unless admin_support_company?
-    else
-      show_or_index?
+    else show_or_index?
     end
   end
 end
