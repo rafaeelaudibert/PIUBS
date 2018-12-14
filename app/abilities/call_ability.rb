d4788b16905f3a366b8c88cffcff9d3539268ca9
RafaAudibert <rafaeelaudibert@gmail.com>
2018-12-14 15:58:12 -0200

    :bug: Fix bug introduced with the call ability addition

--- app/abilities/call_ability.rb
+++ app/abilities/call_ability.rb
@@ -10,8 +10,8 @@ class CallAbility
     # Only the creator can destroy his own call
     can :destroy, Call.where(user_id: user.id) if company_user?(user)
 
-    manage_show_restrictions
-    manage_special_actions
+    manage_show_restrictions(user)
+    manage_special_actions(user)
 
     # Total acess to admin
     can :manage, Call if user.admin?
@@ -19,13 +19,13 @@ class CallAbility
 
   protected
 
-  def manage_show_restrictions
+  def manage_show_restrictions(user)
     can :show, Call if support_user?(user)
     can :show, Call.where(sei: user.sei) if user.company_admin?
     can :show, Call.where(user_id: user.id) if user.company_user?
   end
 
-  def manage_special_actions
+  def manage_special_actions(user)
     can :link_call, Call if support_user?(user)
     can :reopen_call, Call.where(sei: user.sei) if user.company_admin?
     can :reopen_call, Call.where(user_id: user.id) if user.company_user?
