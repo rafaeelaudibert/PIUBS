a6b8346389b6b1f558aef8326207654479e7e50e
RafaAudibert <rafaeelaudibert@gmail.com>
2019-01-02 15:51:48 -0200

    :card_file_box: Change migration code from place

--- db/migrate/20180905130001_add_modules_postgres.rb
+++ db/migrate/20180905130001_add_modules_postgres.rb
@@ -4,10 +4,12 @@ class AddModulesPostgres < ActiveRecord::Migration[5.2]
   def up
     execute 'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
     execute 'CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;'
+    execute 'CREATE EXTENSION IF NOT EXISTS unaccent;'
   end
 
   def down
     execute 'DROP EXTENSION IF EXISTS pg_trgm;'
     execute 'DROP EXTENSION IF EXISTS fuzzystrmatch;'
+    execute 'DROP EXTENSION IF EXISTS unaccent;'
   end
 end
