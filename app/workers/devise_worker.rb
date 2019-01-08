b467b4d821fdc551e8ad7a880d7e0385d81209e8
RafaAudibert <rafaeelaudibert@gmail.com>
2019-01-08 13:22:38 -0200

    :bulb: Documenting source code for DeviseWorker

--- app/workers/devise_worker.rb
+++ app/workers/devise_worker.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+##
+# Worker for the Devise Gem, to perform async mail sending
 class DeviseWorker
   include Sidekiq::Worker
 
+  # Configures Devise to send e-mails in the background,
+  # so that the browser from the user is not blocked,
+  # waiting for a page, while Devise is still sending an e-mail
   def perform(devise_mailer, method, user_id, *args)
     user = User.find(user_id)
     devise_mailer.safe_constantize.send(method, user, *args).deliver_now
-    logger.info "Performing #{method} with user #{User.find(user_id)}"
+    logger.info "[DeviseWorker] Performing #{method} with user #{User.find(user_id)"
   end
 end
