c86d3a9b11e78919e54370099a9a089cb4643e19
RafaAudibert <rafaeelaudibert@gmail.com>
2019-01-08 13:53:57 -0200

    :bulb: Documents WelcomeControlle source code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+##
+# This is a generic controller, to handle the not logged view
 class WelcomeController < ApplicationController
+  ##########################
+  # :section: View methods
+  # Method related to generating views
+
+  # Configures the <tt>index</tt> page. If the user is already
+  # logged in, redirects the user to the logged in dashboard
+  #
+  # <b>ROUTES</b>
+  #
+  # [GET] <tt>/</tt>
   def index
     redirect_to(new_user_session_path) && return if user_signed_in?
     render layout: 'welcome'
